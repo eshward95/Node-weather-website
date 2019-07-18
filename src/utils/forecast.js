d14fb185a528cf57d624b335ea968928fb25326a
eshward95 <eshward95@gmail.com>
2019-07-18 15:55:00 +0530

    Added more information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,7 +13,7 @@ const forecast=(lat,long,callback)=>
         callback("Error for this",undefined)
     }
         {
-            callback(undefined,body.daily.data[0].summary+'It is currently '+body.currently.temperature+' degrees out'+' There is a ' + body.currently.precipProbability + '% chance of rain.'
+            callback(undefined,body.daily.data[0].summary+'It is currently '+body.currently.temperature+' degrees out.'+'The high today is '+body.daily.data[0].temperatureHigh+' with a low of'+' There is a '+body.daily.data[0].temperatureLow + body.currently.precipProbability + '% chance of rain.'
                 )
         }
     })
